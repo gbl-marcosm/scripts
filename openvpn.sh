0c3656fdce71bedade0fd6d3494e20eb1fe6b107
Marcos Morais <marcos.morais@gbl.io>
2022-02-07 22:02:50 -0500

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -4,25 +4,29 @@ set -e
 
 UBUNTU_CODENAME=$(lsb_release --codename | awk -F ' ' '{print $2}')
 
-echo -n "# Checking if OpenVPN client is installed... "
+echo -n "1/2 - Checking if OpenVPN client is installed... "
 INSTALL_CHECK=$(apt -qq list openvpn3 2>/dev/null | grep "openvpn3/$(lsb_release --codename | awk -F ' ' '{print $2}')" | awk -F '[' '{print $2}' | tr -d ']')
 if [ ${INSTALL_CHECK} == "installed" ] && [ -f "/usr/bin/openvpn3" ]; then
     echo "INSTALLED!"
 else
     echo "NOT INSTALLED!"
-    echo -n "# Installing OpenVPN 3 Client... "
-    apt update \
-    && apt install -y apt-transport-https wget \
-    && wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub \
-    && apt-key add openvpn-repo-pkg-key.pub \
-    && rm -f openvpn-repo-pkg-key.pub \
-    && wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-${UBUNTU_CODENAME}.list \
-    && apt update \
-    && apt install -y openvpn3 \
-    && echo "DONE!"
+    echo -n "    - Installing OpenVPN 3 Client... "
+    apt update &>/dev/null \
+    && apt install -y apt-transport-https wget &>/dev/null \
+    && wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub &>/dev/null \
+    && apt-key add openvpn-repo-pkg-key.pub &>/dev/null \
+    && rm -f openvpn-repo-pkg-key.pub &>/dev/null \
+    && wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-${UBUNTU_CODENAME}.list &>/dev/null \
+    && apt update &>/dev/null \
+    && apt install -y openvpn3 &>/dev/null
+    if [ ${INSTALL_CHECK} == "installed" ] && [ -f "/usr/bin/openvpn3" ]; then
+        echo "DONE!"
+    else
+        echo "ERROR!"
+    fi
 fi
 
-echo -n "# Applying fix for OpenVPN DNS service... "
+echo -n "2/2 - Applying fix for OpenVPN DNS service... "
 sed -i 's/--systemd-resolved/--resolv-conf \/etc\/resolv.conf/g' /usr/share/dbus-1/system-services/net.openvpn.v3.netcfg.service \
 && echo "DONE!"
 
